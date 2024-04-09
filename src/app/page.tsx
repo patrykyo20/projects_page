2ca6c576530217cafbabf915758ac930069493c3
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-04-09 14:39:06 +0200

    Initialization Project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,18 @@
-import { currentUser } from '@clerk/nextjs';
+import { SignInButton, SignedIn, SignedOut, UserButton, currentUser } from '@clerk/nextjs';
 
 export default async function Home() {
   const user = await currentUser();
   console.log(user?.id)
 
-  if (!user) return <h1>no user</h1>
-
   return (
-    <h1 className="text-8xl">Hello</h1>
+    <>
+      <h1 className="text-8xl">Hello</h1>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+      <SignedOut>
+        <SignInButton />
+      </SignedOut>
+    </>
   );
 }
