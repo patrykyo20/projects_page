67ab782bef795cc476f8ae22bad63fce56f894d2
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-04-10 13:24:28 +0200

    change main.ts to main.js

---
+++ portfolio-server/main.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import route from './src/routes/api';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.use('', route)
+
+
+app.listen(PORT, () => {
+  console.log(`server is running on localhost:${PORT}`);
+});
+
