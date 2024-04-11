f1367c19ba3a8098f29a85e23daf082c77e7f232
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-04-11 16:25:45 +0200

    create project service

---
+++ portfolio-server/src/services/project.service.ts
@@ -0,0 +1,71 @@
+import Project from "../models/project.model";
+import getRandomId from "../utlis/getRandomId";
+
+const get = async () => {
+  const allProjects = await Project.findAll();
+
+  return allProjects;
+};
+
+const create = async (
+  images: string[],
+  title: string,
+  description: string,
+  technologies: string[],
+  repository: string,
+  linkedin: string,
+  userId: string,
+) => {
+  const id = getRandomId();
+  const likes: string[] = [];
+  const visits = 0;
+
+  return Project.create({
+    id,
+    images,
+    title,
+    likes,
+    visits,
+    description,
+    technologies,
+    repository,
+    linkedin,
+    userId,
+  });
+};
+
+const update = async (
+  id: number,
+  images: string[],
+  title: string,
+  description: string,
+  technologies: string[],
+  repository: string,
+  linkedin: string,
+) => {
+  await Project.update({
+    title,
+    images,
+    description,
+    technologies,
+    repository,
+    linkedin,
+  }, { where: { id } });
+
+  const updatedProject = await Project.findByPk(id);
+  
+  return updatedProject;
+};
+
+const remove = async (id: number) => {
+  await Project.destroy({ where: { id }})
+};
+
+const projectService = {
+  get,
+  create,
+  update,
+  remove,
+};
+
+export default projectService;
