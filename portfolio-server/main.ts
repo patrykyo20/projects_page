ec91353f82b53fddd1488c518c13516f2210f9b2
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-04-11 16:23:42 +0200

    connect with db and use /project endpoint main.ts

--- portfolio-server/main.ts
+++ portfolio-server/main.ts
@@ -1,7 +1,9 @@
 import express, {Express} from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import ApiRouter from './src/routes/api/index';
+import { connection } from './src/utlis/db';
+import errorHandler from './src/utlis/errorHandler';
+import projectRouter from './src/routes/project.router';
 
 dotenv.config();
 
@@ -10,10 +12,13 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 
+connection();
+
+app.use(errorHandler);
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.use('', ApiRouter);
+app.use('/projects', projectRouter);
 
 app.listen(PORT, () => {
   console.log(`server is running on localhost:${PORT}`);
