4e38f97541855b6fd7ef33c8af0ec1b0a26665bc
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-04-12 14:43:46 +0200

    Add colors to theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,11 +7,22 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      primary: '#010314',
+      secondary: '#7241FF',
+      textPrimary: '#77798F',
+      textSecondary: '#ffffff',
+      button: '#2A2B3A',
+      link: '#865BFF'
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      dropShadow: {
+        'button': '0 0 10px rgba(114, 65, 255, 0.4)',
       },
     },
   },
