51569d4484d8f24f74f61befefd540abec35e93a
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-04-12 14:45:17 +0200

    Implement Header to Page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,10 @@
-import { SignInButton, SignedIn, SignedOut, UserButton, currentUser } from '@clerk/nextjs';
+import Button from '@/components/button';
+import Header from './components/Header';
 
 export default async function Home() {
-  const user = await currentUser();
-  console.log(user?.id)
-
   return (
     <>
-      <h1 className="text-8xl">Hello</h1>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
+      <Header />
     </>
   );
 }
