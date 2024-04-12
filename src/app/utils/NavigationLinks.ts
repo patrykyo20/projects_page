12fff12290897fa38497d2f41891b1294eb8613f
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-04-12 14:45:32 +0200

    Create NavigationLinks object in utils

---
+++ src/app/utils/NavigationLinks.ts
@@ -0,0 +1,20 @@
+const navigationLinks = [
+  {
+    link: '/',
+    text: 'Home'
+  },
+  {
+    link: '/portfolios',
+    text: 'Portfolios'
+  },
+  {
+    link: '/community',
+    text: 'Community'
+  },
+  {
+    link: '/about-us',
+    text: 'About us'
+  },
+];
+
+export default navigationLinks;
