00019518c26de8e6ee53a07c8d4d8a0b7cf7fa83
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-04-14 17:55:10 +0200

    add bg color to body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
-        <body>
+        <body className="bg-primary">
           {children}
         </body>
       </html>
