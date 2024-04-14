3b99cbcbeafb26869e1c8a22e7a6903542b71e83
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-04-14 17:54:58 +0200

    Add active props to button component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -4,21 +4,24 @@ interface ButtonProps {
   width: number;
   height: number;
   text: string;
+  active?: boolean;
 };
 
 const Button: FC<ButtonProps> = ({
   width,
   height,
-  text
+  text,
+  active = false
 }) => {
   return (
     <button
-      className="flex justify-center border-4
+      className={`flex justify-center border-4
       items-center text-textSecondary
-      rounded-full border-button bg-primary
-      font-semibold hover:border-secondary
-      hover:drop-shadow-button transition-all
-      ease-in-out duration-500"
+      rounded-full border-button font-semibold
+    hover:border-secondary hover:drop-shadow-button
+      transition-all ease-in-out duration-500 
+      bg-primary
+      ${active ? 'border-secondary drop-shadow-button' : ''}`}
       style={{width: `${width}px`, height: `${height}px`}}
     >
       {text}
