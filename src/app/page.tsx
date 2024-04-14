827a4b50d02045e24e9da5ebeb4e61e71d9f4887
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-04-14 17:58:16 +0200

    create PersonalizeProfil section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Header from './components/Header';
 import Hero from './components/Hero';
-import PersonalizeProfile from './components/PresonalizeProfil';
+import PersonalizeProfile from './components/PersonalizeProfil';
 
 export default function Home() { 
   return (
