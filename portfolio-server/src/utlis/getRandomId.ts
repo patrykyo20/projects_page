53b343f64cfee9b0b3da6245967dcc7c309b0f7c
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-04-29 18:17:02 +0200

    random id functionallity

--- portfolio-server/src/utlis/getRandomId.ts
+++ portfolio-server/src/utlis/getRandomId.ts
@@ -1,6 +1,6 @@
 const getRandomId = () => {
   const min = 1;
-  const max = 10000000000000000000000000000000000000000000;
+  const max = 1000000;
 
   return Math.floor(Math.random() * (max - min + 1)) + 1
 };
