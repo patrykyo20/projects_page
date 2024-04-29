ec13b28e9969e3a0a5aedaab715ee634dd28f2de
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-04-29 18:19:15 +0200

    add user to header

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,12 +1,16 @@
+'use client';
+
 import Button from "@/components/button";
 import Image from "next/image";
 import navigationLinks from "../utils/NavigationLinks";
 import Navigation from "./Navigation";
 import Burger from "./Burger";
-import { SignUpButton, SignInButton } from '@clerk/nextjs';
+import { SignUpButton, SignInButton, useUser } from '@clerk/nextjs';
+import Link from "next/link";
 
 const Header = () => {
-  console.log(SignUpButton)
+  const { user } = useUser();
+
   return (
     <header
       className="px-[40px] lg:px-[50px] lg:py-[33.5px] py-[40px] bg-primary
@@ -21,23 +25,36 @@ const Header = () => {
       <nav className="hidden lg:flex">
         <ul
           className="flex text-textPrimary text-xl font-semibold
-            items-center gap-[40px]"
+            items-center gap-[40px] mr-[80px]"
         >
           {navigationLinks.map(nav => (
             <Navigation key={nav.link} link={nav.link} text={nav.text} />
           ))}
         </ul>
       </nav>
-      <div className="hidden lg:flex gap-[14px]">
-        <SignInButton>
-          <Button width={138} height={51} text={"Sing In"} />
-        </SignInButton>
+      {user 
+        ? 
+          <Link href={`/user-page/${user.id}`}>
+            <Image
+              src={user.imageUrl}
+              alt={"user-logo"}
+              width={50}
+              height={50}
+              className="rounded-full"
+            />
+          </Link>
+        :
+          <div className="hidden lg:flex gap-[14px]">
+            <SignInButton>
+              <Button width={138} height={51} text={"Sing In"} />
+            </SignInButton>
 
-        <SignUpButton>
-          <Button width={138} height={51} text={"Sing Up"} active={true} />
-        </SignUpButton>
-      </div>
-      <Burger />
+            <SignUpButton>
+              <Button width={138} height={51} text={"Sing Up"} active={true} />
+            </SignUpButton>
+          </div>
+        }
+        <Burger />
     </header>
   );
 };
