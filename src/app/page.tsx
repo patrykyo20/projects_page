9bd4aca3d43dc57daa27a5a71eaf73837129e519
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-04-30 18:17:05 +0200

    add projectSection component to main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 import Header from './components/Header';
 import Hero from './components/Hero';
 import PersonalizeProfile from './components/PersonalizeProfil';
+import ProjectSection from './components/ProjectsSection';
 
 export default function Home() { 
   return (
     <>
       <Hero />
       <PersonalizeProfile />
+      <ProjectSection />
     </>
   );
 }
