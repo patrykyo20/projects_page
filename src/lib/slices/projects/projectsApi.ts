c9506435bb9e86c6e71267243a07a0be81be8fb3
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-05-02 10:38:42 +0200

    getProject, getProject and getProjectLength to projectsApi

--- src/lib/slices/projects/projectsApi.ts
+++ src/lib/slices/projects/projectsApi.ts
@@ -1,19 +1,38 @@
+import Project from '@/types/Project';
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-
-interface Project {
-  id: string;
-  title: string;
-  description: string;
-}
 
 export const projectApi = createApi({
   reducerPath: 'projectsApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:5000/projects' }),
   endpoints: (builder) => ({
-    getAllProjects: builder.query<Project[], void>({
-      query: () => 'projects',
+    getAllProjects: builder.query<Project, { page: number; pagePerSize: number; order: string; sort: string }>({
+      query: ({ page, pagePerSize, order, sort }) => ({
+        url: `?page=${page}&pagePerSize=${pagePerSize}&order=${order}&sort=${sort}`,
+      }),    
+    }),
+    getProjectsLength: builder.query({
+      query: () => ({
+        url: '/length',
+      }),    
+    }),
+    getProject: builder.query<Project, {id: number}>({
+      query: (id) => ({
+        url: `/${id}`
+      })
+    }),
+    addLikes: builder.mutation<Project, { id: number; data: Project }>({
+      query: ({ id, data }) => ({
+          url: `/${id}`,
+          method: 'PATCH',
+          body: data,
+      }),
     }),
   }),
 });
 
-export const { useGetAllProjectsQuery } = projectApi;
+export const {
+  useGetAllProjectsQuery,
+  useGetProjectsLengthQuery,
+  useGetProjectQuery,
+  useAddLikesMutation
+} = projectApi;
