b57be2a4e80305e09c740ea5abc0a49bca7c7a57
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-05-04 19:52:31 +0200

    delete header import from main page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import Header from './components/Header';
 import Hero from './components/Hero';
 import PersonalizeProfile from './components/PersonalizeProfil';
 import ProjectSection from './components/ProjectsSection';
