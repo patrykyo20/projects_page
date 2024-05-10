9c67eb5b3f32b1e42049aede71ae44865a32d238
Patrykmclaren14 <wierzbowskipatryk5@gmail.com>
2024-05-10 12:22:32 +0200

    add app use comments to main.ts

--- portfolio-server/main.ts
+++ portfolio-server/main.ts
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import { connection } from './src/utlis/db';
 import errorHandler from './src/utlis/errorHandler';
 import projectRouter from './src/routes/project.router';
+import commentRouter from './src/routes/comment.router';
 
 dotenv.config();
 
@@ -19,6 +20,7 @@ app.use(express.json({limit: '1000mb'}));
 app.use(express.urlencoded({extended: true}));
 
 app.use('/projects', projectRouter);
+app.use('/comments', commentRouter);
 
 app.listen(PORT, () => {
   console.log(`server is running on localhost:${PORT}`);
